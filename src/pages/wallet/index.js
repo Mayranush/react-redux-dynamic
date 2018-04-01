6d4f6e224394dcc026b8b4b152dd5202b72e4092
shushi <shushan.nazaretyan@gmail.com>
2018-04-01 09:49:26 +0400

    table

--- src/pages/wallet/index.js
+++ src/pages/wallet/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import {connect} from "react-redux";
 import {paymentActions, popupActions, projectDataActions} from "../../actions/index";
+import { Pagination } from "../../components/pagination/pagination";
 import "./wallet.scss";
 import PropTypes from "prop-types";
 import {Web3Provider} from "react-web3";
-import {Metamask} from "../../images/metamask.png";
+
 
 let isTransationInProgress = true;
 
@@ -14,6 +15,15 @@ export class Wallet extends React.Component {
     this.handlePay = this.pay.bind(this);
     this.handleShowpopup = this.showPopup.bind(this);
     this.handleTransactionSubmited = this.transactionSubmited.bind(this);
+    this.currentPage = {
+      value: 1
+    };
+    this.itemsInEachPage = 5;
+    this.clickFunction = this.clickFunction.bind(this);
+  }
+
+  clickFunction(item) {
+    this.props.getTransactionsList(item-1, this.itemsInEachPage);
   }
 
   pay(e) {
@@ -67,13 +77,12 @@ export class Wallet extends React.Component {
       transactionReceipt.fromE = transaction.from;
       transactionReceipt.gas = transaction.gas;
       transactionReceipt.nonce = transaction.nonce;
-      transactionReceipt.valueE = parseInt(transaction.v, 16);
+      transactionReceipt.valueE = parseInt(transaction.v,16);
       transactionReceipt.input = transaction.input;
       transactionReceipt.toE = transaction.to;
       transactionReceipt.hashE = hash;
       self.props.addNewTransaction(transactionReceipt);
     });
-
   }
 
   showPopup(message) {
@@ -94,11 +103,11 @@ export class Wallet extends React.Component {
 
 
   componentDidMount() {
-    this.props.getTransactionsList(0, 5);
+    this.props.getTransactionsList(0, this.itemsInEachPage);
   }
 
   render() {
-    console.log(this.props, "props==============")
+    console.log(this.props,"props==============")
     return (
       <div className="main-content">
         <div className="header-section">My wallet</div>
@@ -116,13 +125,53 @@ export class Wallet extends React.Component {
                             placeholder="Input Ether count to charge your account"/></div>
                 <br/>
                 <div className="bot-btn">
-                  <img src={Metamask} onClick={(e) => this.handlePay(e) }>
+                  <button className="btn btn-success dashboard  bot-btn-sub" onClick={(e) => this.handlePay(e) }>
                     Recharge wallet
-                  </img>
+                  </button>
                 </div>
               </div>
             </div>
           </Web3Provider>
+          <div className="table-body">
+              <table className="table">
+                <thead className="thead-inverse">
+                  <tr>
+                    <th>Transaction Hash</th>
+                    <th>Block</th>
+                    <th>Block Hash</th>
+                    <th>From</th>
+                    <th>To</th>
+                    <th>Value</th>
+                    <th>Gas</th>
+                    <th>Nonce</th>
+                    <th>Transaction Index</th>
+                    <th>Input</th>
+                    <th>Status</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.props.data.transactions.length != 0 && this.props.data.transactions.map((item) => {
+                    return ( <tr key={item.id}>
+                      <th scope="row">{item.hashE}</th>
+                      <td>{item.blockNumber}</td>
+                      <td>{item.blockHash}</td>
+                      <td>{item.fromE}</td>
+                      <td>{item.toE}</td>
+                      <td>{item.valueE}</td>
+                      <td>{item.gas}</td>
+                      <td>{item.nonce}</td>
+                      <td>{item.transactionIndex}</td>
+                      <td>{item.input}</td>
+                      <td>{item.statusE}</td>
+                    </tr>)
+                  })
+                  }
+                </tbody>
+              </table>
+              <p className="log-message">{this.props.data.logMessage}</p>
+              {this.props.data.count > 1 &&
+               <Pagination maxPageCount={Math.ceil(this.props.data.count / this.itemsInEachPage)} currentPage={this.currentPage} clickFunction={this.clickFunction}/>}
+          </div>
         </div>
       </div>
     )
